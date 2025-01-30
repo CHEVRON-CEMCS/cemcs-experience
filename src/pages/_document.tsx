fb436ef530189c8e909f1a2728c6ea5561dc814a
DanielOpara <oparadaniv@gmail.com>
2025-01-30 13:25:31 +0100

    pwa removal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json"></link>
-      </Head>
+      <Head>{/* <link rel="manifest" href="/manifest.json"></link> */}</Head>
       <body className="antialiased">
         <Main />
         <NextScript />
